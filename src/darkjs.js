1c5852dfcd1bdd02ea4b8188dc56813c2dbb884c
Jorge Benavides <jorge@agenciaespacial.io>
2021-04-15 21:17:41 -0500

    dark and brigth threshold

--- src/darkjs.js
+++ src/darkjs.js
@@ -4,8 +4,15 @@ import colorjs from './color.js';
 import darkjscss from './darkjs.css';
 
 const style_class = "{class}-{prop}-{level}";
-const style_def = "{path}.{class}.{class}-{prop}-{level}, {path}.{class} .{class}-{prop}-{level} { {prop}: {color} !important; } \n";
-const style_props = ['background-color', 'color', 'border-top', 'border-right-color', 'border-bottom-color', 'border-left-color'];
+const style_def = "{path}.{class}.{style_class}, {path}.{class} .{style_class} { {prop}: {color} !important; } \n";
+const style_props = {
+  'background-color': 'bg', 
+  'color': 'cl',
+  'border-top-color': 'bt',
+  'border-right-color': 'br',
+  'border-bottom-color': 'bb',
+  'border-left-color': 'bl'
+};
 
 class Darkjs {
   constructor(element) {
@@ -15,6 +22,9 @@ class Darkjs {
     this.tree = null;
     this.className = "darkjs";
     this.styles = null;
+    this.offset = 5;
+    this.darkThreshold = 0.3;
+    this.brightThreshold = 0.7;
   }
   getLevels() {
     this.levels = {};
@@ -28,16 +38,20 @@ class Darkjs {
     var black = colorjs("0,0,0");
     for(var i=0; i<keys.length; i++) {
       const level = keys[i];
-      black.lightness = parseFloat((100 - level)/100);
+      black.lightness = parseFloat((100+this.offset - level)/100);
       const rgba = black.toRgba();
       const path = this.root.getPath();
-      style_props.forEach(function(prop, n) {
+      Object.keys(style_props).forEach(function(prop, n) {
         this.styles.addText(parse(style_def, {
-          'prop': prop,
           'path': path,
           'class': this.className,
-          'level': this.levels[level],
-          'color': rgba
+          'style_class': parse(style_class, {
+            'class': this.className,
+            'prop': style_props[prop],
+            'level': this.levels[level]
+          }),
+          'prop': prop,
+          'color': rgba,
         }))
       }.bind(this));
     }
@@ -47,13 +61,13 @@ class Darkjs {
     var levels = [];
     var color;
     
-    style_props.forEach(function(prop, n) {
+    Object.keys(style_props).forEach(function(prop, n) {
       color = colorjs(element.getStyle(prop));
       if(
         this.hasColor(color) &&
         (
-          (n===0 && !this.isDarkColor(color)) ||
-          (n>0 && this.isDarkColor(color))
+          (n===0 && this.is2Bright(color)) ||
+          (n>0 && this.is2Dark(color))
         )
         ) {
         levels.push(this.colorLevel(color));
@@ -67,20 +81,24 @@ class Darkjs {
   }
   setLevelsToElement(element) {
     var color;
-    style_props.forEach(function(prop, n) {
+    Object.keys(style_props).forEach(function(prop, n) {
       color = colorjs(element.getStyle(prop));
       const level = this.levels[this.colorLevel(color)];
       if(
         level && 
         this.hasColor(color) &&
         (
-          (n===0 && !this.isDarkColor(color)) ||
-          (n>0 && this.isDarkColor(color))
+          (n===0 && this.is2Bright(color)) ||
+          (n>0 && this.is2Dark(color))
         )
         ) {
         element
         .addClass(this.className)
-        .addClass(parse(style_class, { 'class': this.className, 'prop': prop, 'level': level}));
+        .addClass(parse(style_class, {
+          'class': this.className,
+          'prop': style_props[prop],
+          'level': level
+        }));
       }
     }.bind(this));
     const childs = element.children;
@@ -119,8 +137,11 @@ class Darkjs {
   hasColor(color) {
     return color.alpha > 0;
   }
-  isDarkColor(color) {
-    return color.alpha > 0.3 && color.lightness < 0.5;
+  is2Dark(color) {
+    return color.lightness < this.darkThreshold;
+  }
+  is2Bright(color) {
+    return color.lightness > this.brightThreshold;
   }
 }
 export default Darkjs;
