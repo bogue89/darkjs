54d779d3b0b14ece4ddefd09ccb93779d95ea8ad
Jorge Benavides <jorge@agenciaespacial.io>
2021-04-22 12:05:55 -0500

    parsing and encoding functions

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,8 +1,31 @@
-export default function parse(string, params) {
+String.prototype.url_encode = function() {
+    return encodeURIComponent(this);
+}
+String.prototype.url_decode = function() {
+    return decodeURIComponent(this);
+}
+Object.prototype.toQueryString = function() {
+    let query = [];
+    Object.keys(this).forEach((key) => {
+        query.push([(key+"").url_encode(), (this[key]+"").url_encode()].join('='));
+    })
+    return query.join('&');
+}
+Object.prototype.fromQueryString = function(string) {
+    let object = {};
+    const params = string.split('&');
+    params.forEach((param) => {
+        const kv = param.split('=');
+        object[kv[0].url_decode()] = kv[1].url_decode();
+    })
+    return object;
+}
+function parse(string, params) {
     const keys = Object.keys(params);
-    for(var i=0; i<keys.length; i++) {
+    for(let i=0; i<keys.length; i++) {
         const key = keys[i];
         string = string.replace(new RegExp("{"+key+"}", "g"), params[key]);
     }
     return string.replace(new RegExp("{\\w+}", "g"), '');
-};
+}
+export default parse;
