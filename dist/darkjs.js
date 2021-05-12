ef69407d4e7645b9f4dc7410f2f8cce193b5f18a
Jorge Benavides <jorge@agenciaespacial.io>
2021-05-12 17:49:00 -0500

    v10.1 dist build

--- dist/darkjs.js
+++ dist/darkjs.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";(()=>{const e=document.querySelector('script[src*="darkjs.js"]'),c=document.createElement("script");c.src=e.src.replace(".js","@1.0.0.js"),document.head.insertBefore(c,e)})()})();
+(()=>{"use strict";(()=>{const e=document.querySelector('script[src*="darkjs.js"]'),c=document.createElement("script");c.src=e.src.replace(".js","@1.0.1.js"),document.head.insertBefore(c,e)})()})();
