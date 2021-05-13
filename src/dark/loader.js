beab119218e59c2d056220001ab9b4faa1871ef0
Jorge Benavides <jorge@agenciaespacial.io>
2021-05-12 22:16:53 -0500

    v1.0.1 dist

--- src/dark/loader.js
+++
@@ -1,6 +0,0 @@
-import _package from '../../package.json';
-
-const s = document.querySelector('script[src*="darkjs.js"]');
-const d = document.createElement('script');
-d.src = s.src.replace('.js', `@${_package.version}.js`);
-document.head.insertBefore(d, s);
